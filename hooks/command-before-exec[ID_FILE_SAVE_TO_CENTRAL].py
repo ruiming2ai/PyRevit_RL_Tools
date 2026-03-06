b95a4aadaf404853e3683f94ad84cb664ca7d751
ruiming2ai <ruiming2ai@outlook.com>
2026-03-06 15:22:19 -0800

    Add one-time save hook proof dialogs

--- hooks/command-before-exec[ID_FILE_SAVE_TO_CENTRAL].py
+++ hooks/command-before-exec[ID_FILE_SAVE_TO_CENTRAL].py
@@ -10,5 +10,6 @@ except Exception:
     _EVENT_ARGS = None
 
 
+close_stop.show_save_hook_probe(event_args=_EVENT_ARGS, hook_name="ID_FILE_SAVE_TO_CENTRAL")
 close_stop.capture_action_command(event_args=_EVENT_ARGS)
 close_stop.handle_save_related_before_exec(event_args=_EVENT_ARGS)
