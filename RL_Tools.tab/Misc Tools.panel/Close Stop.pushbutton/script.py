fd31ea6165221744c1e78c295532d310f62a3a2e
ruiming2ai <ruiming2ai@outlook.com>
2026-03-13 17:25:50 -0700

    Remove Unused Buttons "Close Stop" & "File Save".

--- RL_Tools.tab/Misc Tools.panel/Close Stop.pushbutton/script.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Launch the RL Tools close-stop flow for the active project document."""
-
-from rltools import close_stop
-
-
-if __name__ == "__main__":
-    close_stop.run_pushbutton()
--- RL_Tools.tab/Misc Tools.panel/File Saved.pushbutton/script.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Launch the RL Tools File Saved test popup."""
-
-from rltools import file_saved_notice
-
-
-if __name__ == "__main__":
-    file_saved_notice.run_pushbutton()
